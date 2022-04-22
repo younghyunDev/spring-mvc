5eaa28cdf25c279bfb4cd010036b4a2c3f3426d6
Leeyeonghyeon <dudgus1202@gmail.com>
2022-04-22 15:38:24 +0900

    서블릿 공부

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderService.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="responseHeaderService",urlPatterns = "/responser-header")
+public class ResponseHeaderService extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        resp.setHeader("Content-type","text/plain;charset=utf-8");
+        resp.setHeader("Cache-Control","no-cache,no-store,must-revalidate");
+        resp.setHeader("Pragma","no-cache");
+        resp.setHeader("my-header","hello");
+
+        PrintWriter writer=resp.getWriter();
+        writer.println("안녕하세요;;");
+    }
+
+}
